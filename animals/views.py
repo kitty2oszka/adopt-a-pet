2b65f44b03f8765083717d90a50ec43b237ae738
kitty2oszka <pisseundbenzin@gmail.com>
2026-02-02 02:46:38 +0100

    konfiguracja whitenoise i dodanie bazy sqlite

--- animals/views.py
+++ animals/views.py
@@ -1,13 +1,31 @@
 from django.shortcuts import render
 from .models import Pet
-
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect 
+from django.db.models import Q
 
 def pet_list(request):
+    search_query = request.GET.get('q')
     category = request.GET.get('category')
     
+    pets = Pet.objects.all()
+
     if category:
-        pets = Pet.objects.filter(species=category)
-    else:
-        pets = Pet.objects.all()
+        pets = pets.filter(species=category)
+    if search_query:
+        pets = pets.filter(name__icontains=search_query)    
+
     
     return render(request, 'pet_list.html', {'pets': pets})
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login') 
+    else:
+        form = UserCreationForm()
+    
+    return render(request, 'registration/register.html', {'form': form})
--- config/settings.py
+++ config/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -93,7 +94,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pl'
 
 TIME_ZONE = 'UTC'
 
@@ -106,6 +107,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -113,3 +116,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Przekierowanie po zalogowaniu na stronę główną
 LOGIN_REDIRECT_URL = 'pet_list'
 LOGOUT_REDIRECT_URL = 'pet_list'
+
+
