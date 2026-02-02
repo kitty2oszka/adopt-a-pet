559f1e95bbeb5d74be5058aedd97a34c1670e9af
kitty2oszka <pisseundbenzin@gmail.com>
2026-02-02 03:45:26 +0100

    dodanie wyszukiwarki i pet detail view

--- animals/views.py
+++ animals/views.py
@@ -3,6 +3,7 @@ from .models import Pet
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect 
 from django.db.models import Q
+from django.shortcuts import render, redirect, get_object_or_404
 
 def pet_list(request):
     search_query = request.GET.get('q')
@@ -29,3 +30,8 @@ def register(request):
         form = UserCreationForm()
     
     return render(request, 'registration/register.html', {'form': form})
+
+def pet_detail(request, pk):
+    #pk to Primary Key obiektu Pet
+    pet = get_object_or_404(Pet, pk=pk)
+    return render(request, 'pet_details.html', {'pet': pet})
--- config/urls.py
+++ config/urls.py
@@ -3,12 +3,16 @@ from django.urls import path, include
 from animals.views import pet_list
 from django.conf import settings
 from django.conf.urls.static import static
+from animals.views import register, pet_list, pet_detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')), 
+    path('register/', register, name='register'),
+    path('pet/<int:pk>/', pet_detail, name='pet_detail'),
     path('', pet_list, name='pet_list'),
 ]
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
